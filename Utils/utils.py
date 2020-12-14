ae1e04caaafac2fd09f290e697cbecefa1019097
lima-anderson <msgli34@gmail.com>
2020-12-14 17:28:28 -0300

    opção trecho de música teste

--- Utils/utils.py
+++ Utils/utils.py
@@ -3,11 +3,7 @@ import json
 
 def mostrarOperacoes():
   print('''
-    1) Abrir conta.
-    2) Depósito
-    3) Saldo
-    4) Saque
-    5) Encerrar consulta\n
+    1) Procurar música por trecho.\n
     ''')
   operacao = int(input('Digite o número correspondente a opção desejada: '))
   
@@ -18,41 +14,40 @@ def mostrarOperacoes():
 def escolherOperacao(operacao):
   print('Você digitou ', operacao)
   while True:
-    if operacao == 1: #Abrir conta.
-      nome = input('Digite seu nome: ')
-      senha = int(input('Digite sua senha: '))
+    if operacao == 1: #Procurar música por trecho.
+      trecho = input('Digite seu nome: ')
 
       # dado = str(nome) + ',' + str(senha)
-      dado = json.dumps({"nome": nome, "senha": senha})
+      dado = json.dumps({"trecho": trecho})
       return dado
 
-    elif operacao == 2: # Depósito
-      conta = int(input('Digite o número da conta: '))
-      senha = int(input('Digite sua senha: '))
-      deposito = float(input('Digite o valor: '))
+    # elif operacao == 2: # Depósito
+    #   conta = int(input('Digite o número da conta: '))
+    #   senha = int(input('Digite sua senha: '))
+    #   deposito = float(input('Digite o valor: '))
 
-      # dado = str(conta) + ',' + str(senha) + ',' + str(deposito)
-      dado = json.dumps({"conta": conta, "senha": senha, "deposito": deposito})
-      return dado
+    #   # dado = str(conta) + ',' + str(senha) + ',' + str(deposito)
+    #   dado = json.dumps({"conta": conta, "senha": senha, "deposito": deposito})
+    #   return dado
 
-    elif operacao == 3: # Saldo
-      conta = int(input('Digite o número da conta: '))
-      senha = int(input('Digite sua senha: '))
+    # elif operacao == 3: # Saldo
+    #   conta = int(input('Digite o número da conta: '))
+    #   senha = int(input('Digite sua senha: '))
 
-      # dado = str(conta) + ',' + str(senha)
-      dado = json.dumps({"conta": conta, "senha": senha})
-      return dado
+    #   # dado = str(conta) + ',' + str(senha)
+    #   dado = json.dumps({"conta": conta, "senha": senha})
+    #   return dado
 
-    elif operacao == 4: # Saque
-      conta = int(input('Digite o número da conta: '))
-      senha = int(input('Digite sua senha: '))
-      saque = float(input('Digite o valor: '))
+    # elif operacao == 4: # Saque
+    #   conta = int(input('Digite o número da conta: '))
+    #   senha = int(input('Digite sua senha: '))
+    #   saque = float(input('Digite o valor: '))
       
-      # dado = str(conta) + ',' + str(senha) + ',' + str(saque)
-      dado = json.dumps({"conta": conta, "senha": senha, "saque": saque})
-      return dado
+    #   # dado = str(conta) + ',' + str(senha) + ',' + str(saque)
+    #   dado = json.dumps({"conta": conta, "senha": senha, "saque": saque})
+    #   return dado
 
-    elif operacao == 5: # Encerrar consulta
+    elif operacao == 2: # Encerrar consulta
       sys.exit(0)
       print('Consulta encerrada')
       break
