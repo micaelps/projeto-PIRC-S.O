feb010dd9fbc99b433c530986c5d5e174cdf73a3
micaelps <3.1416micael@gmail.com>
2020-12-18 01:56:40 -0300

    cliente windows

---
+++ Cliente Windows/Menu/menu.py
@@ -0,0 +1,69 @@
+import sys
+from datetime import datetime
+
+def mostrarOperacoes():
+  print('''
+    1) Procurar música por trecho.
+    2) Encerrar consulta\n
+    ''')
+  operacao = input('Digite o número correspondente a opção desejada: ')
+  return operacao
+
+
+def escolherOperacao(operacao):
+  print('Você digitou ', operacao)
+
+  if operacao == '1': #Procurar música por trecho.
+    trecho = input('Digite o trecho: ')
+    dado = f'/ENVIO/13/{trecho}/{len(trecho)}/{datetime.now()}'
+    return dado
+
+  elif operacao == '2': # Encerrar consulta
+    print('Consulta encerrada')
+    sys.exit(0)
+  else:
+    print('Número de operação não existe\n')
+    sys.exit(0)
+
+
+def pegarLista(resposta):
+  resposta = resposta.split('[')[1]
+  resposta = resposta.split(']')[0]
+  lista = resposta.split("',")
+  return lista
+
+
+def mostrarMusicas(resposta):
+  lista = pegarLista(resposta)
+  if lista[0] != '':
+    for i in range(len(lista)):
+      print(f"[{i+1}] - {lista[i].split(' - ')[0]} - {lista[i].split(' - ')[1]}")
+
+    
+
+def pegarLink(resposta):
+  lista = pegarLista(resposta)
+  urls = []
+  if len(lista)>1:
+    for i in range(len(lista)):
+      rota = lista[i].split(' - ')[2]
+      rota = rota.split(': ')
+
+      url = 'https://www.vagalume.com.br'+rota[1]
+      urls.append(url)
+  return urls
+
+
+def escolherMusica(operacao, lista):
+  print('Você digitou ', operacao + 1, '\n')
+  links =pegarLink(lista)
+  if  links== '':
+    print('Opção inválida')
+    return
+  if operacao+1> len(links) or operacao < 0:
+    print('Opção inválida')
+    return
+  else:
+    texto = pegarLink(lista)[operacao]
+    texto = texto.replace("'", "")
+    return texto
---
+++ Cliente Windows/client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import socket
+import sys
+from Menu.menu import *
+from selenium import webdriver
+import time
+
+
+
+HOST = socket.gethostname()     # Endereco IP do Servidor
+PORT = 5000            # Porta que o Servidor esta
+
+if len(sys.argv) > 1:
+    HOST = sys.argv[1]
+
+print('Servidor: ', (HOST, PORT))
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)
+tcp.connect(dest)
+
+print('Para sair use CTRL+X\n')
+
+while True:
+    operacao = mostrarOperacoes()   
+    dado = escolherOperacao(operacao)
+
+    if not dado:
+        break
+    tcp.send(str.encode(dado))
+    dado = tcp.recv(1024)
+    operacao = mostrarMusicas(dado.decode())
+
+tcp.close()
